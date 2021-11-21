54b3131a1a5398194a9c5d2e8aa3eaa6ad0ec071
SirNugget13 <68026241+SirNugget13@users.noreply.github.com>
2021-11-20 17:03:24 -0800

    Added Level 1 Scene + All basic assets for control
    Going to work on the tutorial section when I get time

---
+++ Tank vs Squids/Assets/Scripts/Target.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour //Destroys a specified object when the target is hit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
