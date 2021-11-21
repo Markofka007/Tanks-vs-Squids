a0c7110cd953c2fccaf26733a5c6e5eb95277c61
SirNugget13 <68026241+SirNugget13@users.noreply.github.com>
2021-11-20 19:12:27 -0800

    Added Basic door raising code to Target Script

--- Tank vs Squids/Assets/Scripts/Target.cs
+++ Tank vs Squids/Assets/Scripts/Target.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Target : MonoBehaviour //Destroys a specified object when the target is hit
 {
+    public GameObject objectToBeDestroyed;
+    public float raisedHeight;
+    bool isRaised = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,21 @@ public class Target : MonoBehaviour //Destroys a specified object when the targe
     // Update is called once per frame
     void Update()
     {
+        if(isRaised)
+        {
+            objectToBeDestroyed.gameObject.transform.position += new Vector3(0, 5);
+            isRaised = false;//Mathf.Lerp(gameObject.transform.position.y, raisedHeight, 0.5f)                    
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
         
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            isRaised = true;
+        }
     }
 }
+
