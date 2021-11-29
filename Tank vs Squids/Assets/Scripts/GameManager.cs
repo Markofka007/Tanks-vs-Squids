cbca2c21afd02e1a4c05feedf93428ac620502ef
SirNugget13 <68026241+SirNugget13@users.noreply.github.com>
2021-11-29 00:13:12 -0800

    getting ready to add pause menu

--- Tank vs Squids/Assets/Scripts/GameManager.cs
+++ Tank vs Squids/Assets/Scripts/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI fuelMeter;
     public TextMeshProUGUI enemiesKilledText;
+    public TextMeshProUGUI paused;
+
+    public bool isGamePaused = false;
 
     public int enemiesKilled;
 
@@ -19,6 +23,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isGamePaused)
+            {
+                isGamePaused = false;
+                DisplayPauseUI();
+            }
+        }
+
+    }
+
+    void DisplayPauseUI()
+    {
+        paused.gameObject.SetActive(true);
+        GameObject.Find("Resume Button").SetActive(true);
+        Debug.Log("Test");
+        Debug.Log(GameObject.Find("Resume Button").GetType());
     }
 }
